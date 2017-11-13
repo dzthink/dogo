098fab002b58400b19053947cb5671436753fedd
jason <jason@paramidatech.com>
2017-11-13 18:35:28 +0800

    重新设计context架构

--- Ctx.go
+++ Ctx.go
@@ -1,1 +1,21 @@
 package dogo
+
+import "reflect"
+
+type typeDef struct {
+	t reflect.Type
+
+}
+
+type Ctx struct {
+	typeInstance map[reflect.Type]interface{}
+	idInstance map[string]interface{}
+}
+
+func(ctx *Ctx)GetInstanceWithId(id string) (interface{}, error) {
+	return nil, nil
+}
+
+func(ctx *Ctx)GetInstanceWithType(t reflect.Type)(interface{}, error) {
+	return nil, nil
+}
